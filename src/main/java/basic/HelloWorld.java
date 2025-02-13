e5da1f796b1ec6e240cb7e90863f329e3d21bb35
Pinakpani <joshipinakpani@gmail.com>
2025-02-13 13:00:36 +0530

    Add function added

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello There!!"); //test
+		System.out.println(add(1, 2));
+	}
+	
+	public static int add(int num1, int num2) {
+		return num1+num2;
 	}
 
 }
